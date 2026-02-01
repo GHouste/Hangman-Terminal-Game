e2b76eca1dc9c6cde2b0a34cd713bbfcb576742a
GHouste <imghouste@gmail.com>
2026-02-01 15:18:11 +0100

    added some word for the game and started planning it

--- hangman.py
+++ hangman.py
@@ -1,7 +1,22 @@
 import random
 
+
 def read_file():
+    
+    word_list_txt = open("word list.txt")
+    x = 0
+    for lines in word_list_txt:
+        print(f"{x} {lines}")
+        x = x + 1
+    word_list_txt.close()
+
+def write_to_file():
+    pass
+
+def game_cycle():
     pass
 
 def main():
     pass
+
+read_file()
